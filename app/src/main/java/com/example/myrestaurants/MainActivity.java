296f5631b35985e5389afc5d112cc00e6929c4ae
vmuchui <kaigongimuchui@gmail.com>
2019-07-16 18:41:01 +0300

    add listview and failed espresso tests.

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -3,24 +3,31 @@ package com.example.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public static final String TAG = MainActivity.class.getSimpleName();
     private EditText mLocationEditText;
     private Button mFindRestaurantsButton;
-
+    private TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAppNameTextView = findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(),"fonts/OstrichSans-Heavy.otf");
+        mAppNameTextView.setTypeface(ostrichFont);
         mLocationEditText = findViewById(R.id.locationEditText);
         mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
 
--- app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
@@ -3,18 +3,47 @@ package com.example.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Free tacos","Starbucks","Five Star","Ortiz","Baguettes to the face","Panini","Wine tappers","Meek Steak","Fugo Gaucho","Kenchic inn","Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
         mLocationTextView = findViewById(R.id.locationTextView);
+        mListView = findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this,restaurant,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(),"fonts/OstrichSans-Bold.otf");
+        mLocationTextView.setTypeface(ostrichFont);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         String view = "Here are all the restaurants near: " + location;
